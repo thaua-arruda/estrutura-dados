0609173e46a546e6288bd130d33e456416bd4561
thaua-arruda <thauafarruda@gmail.com>
2024-05-06 14:37:59 -0400

    adicionado exemplo do método indexOf

---
+++ arrays/indexOf.js
@@ -0,0 +1,3 @@
+let array = [1, 2, 3, 4, 5];
+let index = array.indexOf(3);
+console.log(index); // 2, pois o número 3 está no índice 2 (posição dentro do array) e retorna o primeiro elemento encontrado
